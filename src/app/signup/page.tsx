043fa31d1f9640849609b71abfde1260adaca171
João Rodrigues <54764344+nalyx1@users.noreply.github.com>
2023-05-14 15:22:13 -0300

    feat: signup view

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import SignUpForm from '@/components/SignUpForm/page';
+import Wrapper from '@/components/Wrapper';
+
+export default function Login() {
+  return (
+    <Wrapper>
+      <SignUpForm />
+    </Wrapper>
+  );
+}
